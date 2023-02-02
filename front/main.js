650dce0114c0dd26f316e037feb51a0deb6de9c6
Thiago Viana <thiago_rallfall@hotmail.com>
2023-02-02 16:39:12 -0300

    ajustes

--- front/main.js
+++ front/main.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:3000', {
+  const response = await fetch('https://codechat-uyei.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
